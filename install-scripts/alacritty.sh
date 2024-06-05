b62bccf437f4b9279781a8145e5c9870fc6f0bcd
prLorence <paulorencereyes@gmail.com>
2024-06-06 00:04:57 +0800

    symlink config

--- install-scripts/alacritty.sh
+++ install-scripts/alacritty.sh
@@ -1,4 +1,5 @@
 BUILD_PATH=$HOME/Desktop/Applications
+BASE_DIR=$(dirname $(pwd))
 # install depedencies
 apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
@@ -36,3 +37,6 @@ scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1
 scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
 scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
 scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+
+# symlink config
+sudo -sf $BASE_DIR/alacritty $HOME/.config/alacritty
