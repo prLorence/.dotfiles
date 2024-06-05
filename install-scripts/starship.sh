d0a23da03fbeb171ace01641ca13c7154c8b3be5
prLorence <paulorencereyes@gmail.com>
2024-06-06 00:05:12 +0800

    fix symlink directory

--- install-scripts/starship.sh
+++ install-scripts/starship.sh
@@ -5,6 +5,4 @@ BASE_DIR=$(dirname $(pwd))
 curl -sS https://starship.rs/install.sh | sh
 
 # symlink the config path to ~/.config/starship/starship.toml
-mkdir -p $HOME/.config/starship
-
-sudo ln -sf $BASE_DIR/starship/starship.toml $HOME/.config/starship/starship.toml
+sudo ln -sf $BASE_DIR/starship $HOME/.config/starship
