ddeeb829604796967f520d6f704fbd36d348885b
prLorence <paulorencereyes@gmail.com>
2024-06-06 12:40:10 +0800

    include zplug installer, fix installation order

--- install-scripts/installer.sh
+++ install-scripts/installer.sh
@@ -15,26 +15,29 @@ chmod +x *.sh
 echo "installing discord..."
 /bin/bash discord.sh
 
+echo "installing spotify..."
+/bin/bash spotify.sh
+
 # echo "installing logseq..."
 # /bin/bash logseq.sh
+
+echo "installing alacritty..."
+/bin/bash alacritty.sh
 
 echo "installing neovim..."
 /bin/bash neovim.sh
 
-echo "installing spotify..."
-/bin/bash spotify.sh
-
 echo "installing zsh..."
 /bin/bash zsh.sh
-
-echo "installing solaar..."
-/bin/bash solaar.sh
 
 echo "installing starship..."
 /bin/bash starship.sh
 
-echo "installing alacritty..."
-/bin/bash alacritty.sh
+echo "installing zplug..."
+/bin/bash zplug.sh
+
+echo "installing solaar..."
+/bin/bash solaar.sh
 
 echo "installing tmux..."
 /bin/bash tmux.sh
