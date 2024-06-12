b83f4551e79cab7df369cb041a9d80db8921a758
prLorence <paulorencereyes@gmail.com>
2024-06-12 16:11:15 +0800

    added cleanups for deb installation packages

--- install-scripts/discord.sh
+++ install-scripts/discord.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt install unzip
-
 wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
 
-sudo apt install discord.deb -y
+sudo apt install ./discord.deb -y
+
+rm discord.deb
