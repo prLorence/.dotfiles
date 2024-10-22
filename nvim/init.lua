ca095841943ae6aca0b4414c805697464908e29b
prLorence <paulorencereyes@gmail.com>
2024-10-22 12:37:15 +0800

    remove autocmd for markdown

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@
 
 require 'phetoush.set'
 require 'phetoush.remap'
-require 'phetoush.autocmd'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
